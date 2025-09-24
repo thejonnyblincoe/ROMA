7db24fd4f2a771e07c88ae10a4f1e32e60b4bcdf
salzubi401 <salaheddin@sentient.xyz>
2025-09-23 21:33:44 -0400

    minimum solver stuff involved only

--- src/roma_dspy/engine/__init__.py
+++ src/roma_dspy/engine/__init__.py
@@ -1,17 +1,11 @@
 """Engine utilities for ROMA DSPy hierarchical execution."""
 
 from .dag import TaskDAG
-from .dag_executor import DAGExecutor
 from .solve import RecursiveSolver, solve, async_solve
-from .tracker import ExecutionTracker
-from .visualizer import DAGVisualizer
 
 __all__ = [
     "TaskDAG",
-    "DAGExecutor",
     "RecursiveSolver",
     "solve",
     "async_solve",
-    "ExecutionTracker",
-    "DAGVisualizer",
 ]
